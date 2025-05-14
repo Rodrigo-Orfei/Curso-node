07dcfef8313a30b6f0c58346c445e530485bdf96
Rodrigo Orfei <generoso.rodrigo07@gmail.com>
2025-05-14 20:05:35 -0300

    Concluída a aula 10

---
+++ 01-fundamentos-nodejs/streams/fake-upload-to-http-stream.js
@@ -0,0 +1,29 @@
+import { Readable } from "node:stream"
+
+class OneToHundredStream extends Readable {
+    index = 1
+    _read() {
+        const i = this.index++
+
+        if (i > 100) {
+            this.push(null)
+        } else {
+            var buf = Buffer.from(String(i)) // Convertendo para Buffer
+        }
+
+        setTimeout(() => {
+            const canContinue = this.push(buf);
+            if (canContinue) {
+                this._read();
+            }
+        }, 1000)
+    }
+}
+
+const stream = Readable.toWeb(new OneToHundredStream());
+
+fetch('http://localhost:8081', {
+    method: 'POST',
+    body: stream,
+    duplex: 'half'
+})
